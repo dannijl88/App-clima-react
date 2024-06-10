513d138656650350ebfb81c6a3a74e85ced5a279
DanielJuan <dannijl88web@gmail.com>
2024-06-10 17:06:58 +0200

    Adaptación a móvil

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="bg-sky-600 h-screen flex flex-col gap-10 justify-center items-center">
       <h1 className="text-center text-5xl text-white font-bold">Weather App React</h1>
-      <div className="bg-gray-800 border-2 w-[90%] md:w-1/2 border-gray-200 p-4 rounded-lg">
+      <div className="bg-gray-800 border-2 w-[90%] max-w-screen-md md:w-1/2 border-gray-200 p-4 rounded-lg">
         <form className="flex flex-col gap-2 mb-4" onSubmit={(e)=>{e.preventDefault();getWeather()}}>
           <input className="text-center h-10" required type="text" placeholder="Introduce Ciudad" onChange={(e)=>setCity(e.target.value)} />
           <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
